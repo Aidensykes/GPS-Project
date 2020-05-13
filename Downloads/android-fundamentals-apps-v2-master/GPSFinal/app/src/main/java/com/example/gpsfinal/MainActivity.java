135dae7d5db3c13823fb695f7fd7feab346a8548
Aiden Sykes <a.sykes5222@studentleedsbeckett.ac.uk>
2020-05-13 17:48:54 +0100

    Debug Location working

--- Downloads/android-fundamentals-apps-v2-master/GPSFinal/app/src/main/java/com/example/gpsfinal/MainActivity.java
+++ Downloads/android-fundamentals-apps-v2-master/GPSFinal/app/src/main/java/com/example/gpsfinal/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.gpsfinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Save state on startup
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= 23){
+
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                // Request Location of GPS tracker
+                // Permissions = To Access user's GPS
+
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            } else {
+                    // Request permission
+                startService();
+            }
+                    // Request Location
+
+        } else {
+
+            //start location services
+            startService();
+        }
+
+
+
+    }
+
+    void startService(){
+        Intent intent = new Intent(MainActivity.this, LocationService.class);
+        startService(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startService();
+                }
+                else{
+                    Toast.makeText(this, "Give permissions to start", Toast.LENGTH_LONG).show();
+                }
+        }
     }
 }
+//setContentView(R.layout.activity_main);
